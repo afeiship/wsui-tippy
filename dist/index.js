bf7326b88da666d6e9d666a16c840d3021e6a9b4
afeiship <1290657123@qq.com>
2021-01-01 19:33:29 +0800

    feat: initial

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+/*!
+ * name: @jswork/wsui-tippy
+ * description: Theme for tippyjs style.
+ * homepage: https://github.com/afeiship/wsui-tippy
+ * version: 1.0.0
+ * date: 2020-11-14T05:31:01.746Z
+ * license: MIT
+ */
+
+module.exports = function(params) {
+  console.log(params);
+};
